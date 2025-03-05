5982092ef23f7cdecbcd15da30175ecb09ac6d7d
bmbmbm9 <bmbmbm9@protonmail.com>
2025-03-06 00:54:34 +0200

    Initial commit

---
+++ src/main/java/knu/fit/kbzi/sydorenko/antibotdefender/AntiBotDefenderApplication.java
@@ -0,0 +1,13 @@
+package knu.fit.kbzi.sydorenko.antibotdefender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntiBotDefenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AntiBotDefenderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/knu/fit/kbzi/sydorenko/antibotdefender/AntiBotDefenderApplicationTests.java
@@ -0,0 +1,13 @@
+package knu.fit.kbzi.sydorenko.antibotdefender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntiBotDefenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
