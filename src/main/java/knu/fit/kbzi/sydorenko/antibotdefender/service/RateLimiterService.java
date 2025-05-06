18f6ba36c0fed7486b485833254883cacb9f5765
bsydorenko7 <sidorenko2002bogdan@gmail.com>
2025-05-06 16:12:27 +0300

    added cache for rate limiter

--- src/main/java/knu/fit/kbzi/sydorenko/antibotdefender/service/RateLimiterService.java
+++ src/main/java/knu/fit/kbzi/sydorenko/antibotdefender/service/RateLimiterService.java
@@ -1,25 +1,28 @@
 package knu.fit.kbzi.sydorenko.antibotdefender.service;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class RateLimiterService {
+
     private static final int MAX_REQUESTS = 5;
     private static final long TIME_WINDOW_MILLIS = 10_000;
 
-    private final Map<String, Deque<Long>> requestTimestamps = new ConcurrentHashMap<>();
+    private final Cache<String, Deque<Long>> requestCache = Caffeine.newBuilder()
+            .expireAfterAccess(5, TimeUnit.MINUTES)
+            .maximumSize(10_000)
+            .build();
 
     public boolean isAllowed(String ip) {
         long now = Instant.now().toEpochMilli();
-        requestTimestamps.putIfAbsent(ip, new ArrayDeque<>());
-
-        Deque<Long> timestamps = requestTimestamps.get(ip);
+        Deque<Long> timestamps = requestCache.get(ip, k -> new ArrayDeque<>());
 
         synchronized (timestamps) {
             while (!timestamps.isEmpty() && now - timestamps.peekFirst() > TIME_WINDOW_MILLIS) {
